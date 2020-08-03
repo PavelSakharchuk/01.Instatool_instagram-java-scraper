2cfefabcbbd5e015eba20489e5bba88b5382545a
Pavel Sakharchuk <sakharchuk.pavel@gmail.com>
2020-08-04 00:13:10 +0300

    deleting extra RandomInstaTest

--- src/test/java/me/postaddict/instagram/scraper/RandomInstaTest.java
+++ src/test/java/me/postaddict/instagram/scraper/RandomInstaTest.java
@@ -2,13 +2,6 @@ package me.postaddict.instagram.scraper;
 
 import me.postaddict.instagram.scraper.client.InstaClient;
 import me.postaddict.instagram.scraper.client.InstaClientFactory;
-import me.postaddict.instagram.scraper.cookie.CookieHashSet;
-import me.postaddict.instagram.scraper.cookie.DefaultCookieJar;
-import me.postaddict.instagram.scraper.exception.InstagramAuthException;
-import me.postaddict.instagram.scraper.interceptor.ErrorInterceptor;
-import me.postaddict.instagram.scraper.interceptor.FakeBrowserInterceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -23,28 +16,13 @@ public class RandomInstaTest {
         instagram = new Instagram(instaClient);
     }
 
-    @Test(expected = InstagramAuthException.class)
-    public void testLoginWithInvalidCredentials() throws Exception {
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS);
-        OkHttpClient httpClient = new OkHttpClient.Builder()
-                .addNetworkInterceptor(loggingInterceptor)
-                .addInterceptor(new FakeBrowserInterceptor(InstaClientFactory.UserAgent.randomUserAgent().getValue()))
-                .addInterceptor(new ErrorInterceptor())
-                .cookieJar(new DefaultCookieJar(new CookieHashSet()))
-                .build();
-        Instagram instagramClient = new Instagram(new InstaClient(httpClient));
-        instagramClient.basePage();
-        instagramClient.login("1", "2");
-    }
-
     @Test
     public void testGetRandomClient() {
-        InstaClient instaClient = new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
-        instaClient = new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
-        instaClient = new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
-        instaClient = new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
-        instaClient = new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
+        new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
+        new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
+        new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
+        new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
+        new InstaClientFactory(InstaClient.InstaClientType.randomClientType()).getClient();
     }
 
 }
